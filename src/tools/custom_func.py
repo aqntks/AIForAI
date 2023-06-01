e500440f7d9ece7b5510eb9201418a45ec0b17fc
aqntks <cjd0030@gmail.com>
2023-06-01 23:18:22 +0900

    Add a function that returns save_hugging_face_tool.

--- src/tools/custom_func.py
+++ src/tools/custom_func.py
@@ -1,7 +1,7 @@
 
 from typing import Any
 
-from langchain.tools import tool
+from langchain.tools import tool, Tool
 
 
 @tool
@@ -11,3 +11,16 @@ def save_huggingface_model(save_path: str, model: Any) -> str:
     model.save_pretrained(save_path)
 
     return f"{save_path} : Model saved successfully."
+
+
+def save_huggingface_model_tool() -> Tool:
+    """
+    return save_hugging_face Tool
+
+    :return: Tool
+    """
+    return Tool(
+            name="save_huggingface_model",
+            func=save_huggingface_model,
+            description="useful for when you need to save huggingface model to a specific path"
+    )
