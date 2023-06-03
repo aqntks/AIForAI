5cb11e1bab2aae1b8bb308a168ff78d1535c6cd3
aqntks <cjd0030@gmail.com>
2023-06-03 23:01:30 +0900

    add python tool

--- src/tools/custom_func.py
+++ src/tools/custom_func.py
@@ -2,6 +2,7 @@
 from typing import Any
 
 from langchain.tools import tool, Tool
+from langchain.utilities import PythonREPL
 
 
 @tool
@@ -23,4 +24,20 @@ def save_huggingface_model_tool() -> Tool:
             name="save_huggingface_model",
             func=save_huggingface_model,
             description="useful for when you need to save huggingface model to a specific path"
+    )
+
+
+def python_tool() -> Tool:
+    """
+    return python Tool
+
+    :return: Tool
+    """
+
+    python_repl = PythonREPL()
+
+    return Tool(
+            name="python_repl",
+            description="A Python shell. Use this to execute python commands. Input should be a valid python command. If you want to see the output of a value, you should print it out with `print(...)`.",
+            func=python_repl.run
     )
