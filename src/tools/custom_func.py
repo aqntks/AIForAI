cc38790be92c63ba4440956f8b3505497a54584a
aqntks <cjd0030@gmail.com>
2023-06-10 00:43:29 +0900

    add load_tokenizer tool

--- src/tools/custom_func.py
+++ src/tools/custom_func.py
@@ -3,6 +3,11 @@ from typing import Any
 
 from langchain.tools import tool, Tool
 from langchain.utilities import PythonREPL
+from transformers import (
+    AutoModel,
+    TrainingArguments,
+    Trainer
+)
 
 
 @tool
@@ -24,6 +29,30 @@ def save_huggingface_model_tool() -> Tool:
             name="save_huggingface_model",
             func=save_huggingface_model,
             description="useful for when you need to save huggingface model to a specific path"
+    )
+
+
+@tool
+def load_tokenizer(query: str) -> str:
+    """useful for when you need to load tokenizer"""
+
+    from transformers import AutoTokenizer
+
+    tokenizer = AutoTokenizer.from_pretrained(query)
+
+    return tokenizer
+
+
+def load_tokenizer_tool() -> Tool:
+    """
+    return load_tokenizer Tool
+
+    :return: Tool
+    """
+    return Tool(
+            name="load_tokenizer",
+            func=load_tokenizer,
+            description="useful for when you need to load tokenizer"
     )
 
 
